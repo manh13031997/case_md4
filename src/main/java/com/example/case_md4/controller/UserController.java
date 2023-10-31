39396b8cb5594255856ed030d5e55dee1487a932
manh13031997 <nguyenducmanh130397@gmail.com>
2023-10-31 14:29:48 +0700

    create

--- src/main/java/com/example/case_md4/controller/UserController.java
+++ src/main/java/com/example/case_md4/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.case_md4.controller;
 
+import com.example.case_md4.model.User;
 import com.example.case_md4.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,6 +21,19 @@ public class UserController {
     public ModelAndView showUser(){
         ModelAndView modelAndView = new ModelAndView("user/userList");
         modelAndView.addObject("manh", userService.findAll());
+        return modelAndView;
+    }
+    @GetMapping("/userCreate")
+    public ModelAndView showCreateForm(){
+        ModelAndView modelAndView = new ModelAndView("/user/userCreate");
+        modelAndView.addObject("user", new User());
+        return modelAndView;
+    }
+
+    @PostMapping("userCreate")
+    public ModelAndView save(User user){
+        userService.save(user);
+        ModelAndView modelAndView= new ModelAndView("redirect:/user");
         return modelAndView;
     }
 }
